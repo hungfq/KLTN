7bfc458c5a716135b25e2080ba63a345c7fcd043
baobeta <bao.lq@holistics.io>
2023-03-01 15:46:01 +0700

    Add daisyui for ui

--- packages/frontend/src/components/Student/ManageTopicResult.vue
+++ packages/frontend/src/components/Student/ManageTopicResult.vue
@@ -4,6 +4,33 @@
       Hiện tại đang không có đợt kết quả đăng ký, vui lòng chọn mục khác!
     </div>
   </template>
+  <label
+    for="my-modal"
+    class="btn"
+  >open modal</label>
+
+  <!-- Put this part before </body> tag -->
+  <input
+    id="my-modal"
+    type="checkbox"
+    class="modal-toggle"
+  >
+  <div class="modal">
+    <div class="modal-box">
+      <h3 class="font-bold text-lg">
+        Congratulations random Internet user!
+      </h3>
+      <p class="py-4">
+        You've been selected for a chance to get one year of subscription to use Wikipedia for free!
+      </p>
+      <div class="modal-action">
+        <label
+          for="my-modal"
+          class="btn"
+        >Yay!</label>
+      </div>
+    </div>
+  </div>
   <template v-if="open">
     <div
       class="shadow-md sm:rounded-lg m-4"
