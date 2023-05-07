a8943d92389d4fb3ee540755204cbc6e9b988d57
Hung Pham <t.hungpham2314@gmail.com>
2023-05-07 22:34:49 +0700

    [BE] topic: view - fix filter

--- packages/backend_php/app/Modules/Topic/DTO/TopicViewDTO.php
+++ packages/backend_php/app/Modules/Topic/DTO/TopicViewDTO.php
@@ -39,6 +39,7 @@ class TopicViewDTO extends FlexibleDataTransferObject
             'is_critical_approve' => $request->input('is_critical_approve'),
             'as_least_lecturer_approve' => $request->input('as_least_lecturer_approve'),
             'is_lecturer_mark' => $request->input('is_lecturer_mark'),
+            'is_critical_mark' => $request->input('is_critical_mark'),
             'is_president_mark' => $request->input('is_president_mark'),
             'is_secretary_mark' => $request->input('is_secretary_mark'),
             'limit' => $request->input('limit'),
