f817b59be74e02b8811faedb240d018642df012a
Hung Pham <t.hungpham2314@gmail.com>
2023-05-31 09:30:08 +0700

    [BE] - fix BE

--- packages/backend_php/app/Modules/Committee/Actions/CommitteeShowAction.php
+++ packages/backend_php/app/Modules/Committee/Actions/CommitteeShowAction.php
@@ -11,14 +11,14 @@ CommitteeShowAction
     {
         $query = Committee::query()
             ->where('committees.id', $id)
-            ->with(['president', 'secretary', 'critical', 'topics' => function ($query) {
-                $query->select('*');
-            }]);
+            ->with(['president', 'secretary', 'critical', 'topics']);
+
         $query->addSelect([
             $query->qualifyColumn('*'),
             'uc.name as created_by_name',
             'uu.name as updated_by_name',
         ]);
+
         $query->leftJoin('users as uc', 'uc.id', '=', $query->qualifyColumn('created_by'))
             ->leftJoin('users as uu', 'uu.id', '=', $query->qualifyColumn('updated_by'));
 
