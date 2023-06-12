e5cd61052019c746fdbe611d7d52239d89246ba6
baobeta <lequocbao29072001@gmail.com>
2023-06-12 15:30:29 +0700

    FE: fix form approve proposal lecturer

--- packages/frontend/src/components/Lecturer/ManageTopicProposal/FormApproveProposalV2.vue
+++ packages/frontend/src/components/Lecturer/ManageTopicProposal/FormApproveProposalV2.vue
@@ -8,93 +8,180 @@
   </div>
   <div class="p-4 w-full h-full md:h-auto mx-auto mt-[10px]">
     <!-- Modal content -->
-    <div class="bg-white rounded-lg shadow">
-      <!-- Modal header -->
+    <div class="bg-white rounded-lg shadow pb-4">
       <div class="flex justify-between items-start p-4 rounded-t border-b">
         <h3 class="text-xl font-semibold text-gray-900">
-          Thông tin đề tài
+          Thông tin đề xuất
         </h3>
       </div>
-      <div class="ml-5 grid grid-cols-2">
-        <FormKit
-          v-model="title"
-          type="text"
-          name="title"
-          label="Tiêu đề"
-          help="Vd: Xây dụng web thương mại điện tử e-shop"
-          validation="required"
-          :validation-messages="{ required: 'Vui lòng điền thông tin vào ô này' }"
-          :disabled="isView"
-        />
-        <FormKit
-          v-model="limit"
-          name="limit"
-          type="number"
-          label="Số thành viên"
-          validation="min:1|max:3"
-          :disabled="isView"
-          :validation-messages="{ min: 'Phải có ít nhất 1 thành viên', max:'Có tối đa 3 thành viên' }"
-        />
+      <template v-if="!loading">
         <div
-          v-show="false"
-          class="w-3/4"
+          class="ml-5 mt-5 flex flex-col items-center"
         >
-          <span class="font-bold text-sm">
-            Giảng viên hướng dẫn
-          </span>
-          <div class="mt-1">
-            <Multiselect
-              v-model="lecturerId"
-              :can-clear="false"
-              :can-deselect="false"
-              no-results-text="Không có kết quả"
-              no-options-text="Không có lựa lựa chọn"
-              :options="listLecturers"
-              :close-on-select="false"
-              :disabled="isView"
-            />
+          <template v-if="page ===1 || isView">
+            <div class="flex">
+              <FormKit
+                v-model="title"
+                type="text"
+                name="title"
+                label="Tiêu đề"
+                help="Vd: Xây dụng web thương mại điện tử e-shop"
+                validation="required"
+                outer-class="!mx-8 w-[400px]"
+                :validation-messages="{ required: 'Vui lòng điền thông tin vào ô này' }"
+
+                :disabled="isView"
+              />
+              <FormKit
+                v-model="description"
+                name="description"
+                type="textarea"
+                label="Mô tả"
+                help="Ghi các thông tin chi tiết tại đây"
+                validation="required"
+                outer-class="!mx-8 w-[400px]"
+                :validation-messages="{ required: 'Vui lòng điền thông tin vào ô này' }"
+                :disabled="isView"
+              />
+            </div>
+            <div class="flex">
+              <div
+                class="w-[400px] !mx-8"
+              >
+                <span class="font-bold text-sm">
+                  Giảng viên hướng dẫn
+                </span>
+                <div class="mt-1">
+                  <Multiselect
+                    v-model="lecturerId"
+                    :can-clear="false"
+                    :options="listLecturers"
+                    :disabled="isView || isUpdate"
+                  />
+                </div>
+              </div>
+              <div
+                class="w-[400px] !mx-8"
+              >
+                <span class="font-bold text-sm">
+                  Đợt đăng ký
+                </span>
+                <div class="mt-1">
+                  <Multiselect
+                    v-model="scheduleId"
+                    :options="scheduleSelect"
+                    :can-deselect="false"
+                    no-results-text="Không có kết quả"
+                    no-options-text="Không có lựa lựa chọn"
+                    :can-clear="false"
+                    :searchable="true"
+                    :disabled="isView || isUpdate"
+                  />
+                </div>
+              </div>
+            </div>
+          </template>
+          <template v-if="page===2 || isView">
+            <div class="flex flex-col">
+              <div class="font-bold my-4">
+                Danh sách sinh viên đã chọn
+              </div>
+              <div class="overflow-x-auto w-[600px]">
+                <EasyDataTable
+                  show-index
+                  :headers="headers"
+                  :items="listStudentsSelected"
+                  :loading="loading"
+                  :buttons-pagination="false"
+                  hide-footer
+                />
+              </div>
+            </div>
+            <div
+              v-if="!isView"
+              class="flex mt-4"
+            >
+              <FormKit
+                v-model.number="limit"
+                name="limit"
+                type="number"
+                label="Số thành viên"
+                outer-class="!mx-8 w-[300px]"
+                :validation-messages="{ required: 'Vui lòng điền thông tin vào ô này', min: 'Số lượng không nhỏ hơn 1', max: 'Số lượng không lớn hơn 3' }"
+                validation="required|min:1|max:3"
+                :disabled="isView"
+              />
+              <button
+                v-if="!isView"
+                class="btn btn-primary mt-5 !mx-8 w-[250px]"
+                @click="chooseStudent"
+              >
+                Chọn danh sách sinh viên
+              </button>
+            </div>
+          </template>
+          <div
+            v-if="!isView"
+            class="my-4 w-[300px] mx-auto mt-8"
+          >
+            <ul class="steps">
+              <li
+                v-for="(step, index) in steps"
+                :key="index"
+                class="step"
+                :class="{ 'step-primary': page >= step.page }"
+                @click="page=step.page"
+              >
+                {{ step.label }}
+              </li>
+            </ul>
           </div>
         </div>
-        <div class="my-2-1 w-3/4">
-          <span class="font-bold text-sm py-4 my-4">
-            Sinh viên đăng kí
-          </span>
-          <div class="mt-1">
-            <Multiselect
-              v-model="studentIds"
-              mode="tags"
-              :close-on-select="false"
-              :searchable="true"
-              :options="listStudents"
-              :disabled="isView"
-              :limit="limit"
-            />
-          </div>
+        <!-- Modal footer -->
+        <div class="mt-10 flex items-center p-4 justify-end rounded-b border-t border-gray-200">
+          <button
+            v-if="!isView && page === 2"
+            type="button"
+            class="btn btn-secondary mx-2"
+            @click="page=1"
+          >
+            Quay lại
+          </button>
+          <button
+            v-if="!isView && page !== 1"
+            type="button"
+            class="btn btn-primary mx-2"
+            :disabled="!isValid"
+            @click="handleAddTopicAdmin"
+          >
+            {{ isSave ? 'Lưu' : 'Cập nhật' }}
+          </button>
+          <button
+            v-if="!isView && page === 1"
+            type="button"
+            class="btn btn-primary mx-2"
+            @click="page=2"
+          >
+            Tiếp theo
+          </button>
         </div>
-        <FormKit
-          v-model="description"
-          name="description"
-          type="textarea"
-          label="Mô tả"
-          help="Ghi các thông tin chi tiết tại đây"
-          :validation-messages="{ required: 'Vui lòng điền thông tin vào ô này' }"
-          validation="required"
-          :disabled="isView"
-        />
-      </div>
-      <!-- Modal footer -->
-      <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200">
-        <button
-          v-if="!isView"
-          type="button"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4  focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          @click="handleAddTopicAdmin"
-        >
-          {{ isSave ? 'Lưu' : 'Phê duyệt' }}
-        </button>
+      </template>
+      <div
+        v-else
+        class="mb-4"
+      >
+        <LoadingProcess />
       </div>
     </div>
   </div>
+  <SelectStudent
+    v-model="showSelectStudent"
+    :schedule-id="scheduleId"
+    :selected="listStudentsSelected"
+    :enabled-excel="true"
+    :type="'TOPIC_PROPOSAL'"
+    @change-students="changeStudents"
+  />
 </template>
 
 <script>
@@ -103,11 +190,13 @@ import { getValidationMessages } from '@formkit/validation';
 import { mapState, mapGetters } from 'vuex';
 import ScheduleApi from '../../../utils/api/schedule';
 import TopicProposalApi from '../../../utils/api/topic_proposal';
+import SelectStudent from '../../Modal/SelectStudent.vue';
 
 export default {
   name: 'FormApproveProposal',
   components: {
     Multiselect,
+    SelectStudent,
   },
   props: {
   },
@@ -133,6 +222,21 @@ export default {
       ],
       messages: '',
       listSchedules: [],
+      deadline: '',
+      listStudentsSelected: [],
+      page: 1,
+      steps: [
+        { label: 'Thông tin cho đề tài', page: 1 },
+        { label: 'Chọn sinh viên', page: 2 },
+      ],
+      loading: false,
+      showSelectStudent: false,
+      selectStudentScheduleId: null,
+      headers: [
+        { text: 'Mã số', value: 'code', sortable: true },
+        { text: 'Tên ', value: 'name', sortable: true },
+        { text: 'Email', value: 'email' },
+      ],
     };
   },
   computed: {
@@ -154,45 +258,64 @@ export default {
     isView () {
       return this.section === 'topic_proposal_approve-view';
     },
+    scheduleSelect () {
+      return this.listSchedules.map((s) => ({
+        value: s._id,
+        label: s.name,
+      }));
+    },
+    isValid () {
+      if (!this.title) {
+        return false;
+      }
+      if (!this.limit) {
+        return false;
+      }
+      if (Number(this.limit) < 1 || Number(this.limit) > 3) {
+        return false;
+      }
+      if (this.studentIds.length !== this.limit) {
+        return false;
+      }
+      return true;
+    },
   },
   async mounted () {
-    await this.$store.dispatch('student/fetchListStudent', this.token);
-    const students = this.$store.state.student.listStudents;
-    this.listLecturers = [{
-      value: this.userInfo._id,
-      label: this.userInfo.name,
-      disabled: true,
-    }];
-    this.listStudents = students.map((student) => {
-      let st = {
-        value: student.code,
-        label: `${student.name} - ${student.code}`,
-      };
-      if (this.isView) {
-        st = { ...st, disabled: true };
-      }
-      return st;
-    });
+    try {
+      await this.$store.dispatch('student/fetchListStudent', this.token);
+      // const students = this.$store.state.student.listStudents;
+      this.listLecturers = [{
+        value: this.userInfo._id,
+        label: this.userInfo.name,
+        disabled: true,
+      }];
 
-    if (this.isUpdate || this.isView) {
-      const { id } = this.$store.state.url;
-      const { listTopicProposalByLecturer } = this.$store.state.topic_proposal;
-      const topic = listTopicProposalByLecturer.find((s) => s._id.toString() === id.toString());
-      if (topic) {
-        this.title = topic.title;
-        this.code = topic.code;
-        this.description = topic.description;
-        this.limit = topic.limit;
-        if (topic.lecturerId) {
-          this.lecturerId = topic.lecturerId;
+      if (this.isUpdate || this.isView) {
+        const { id } = this.$store.state.url;
+        const topic = await TopicProposalApi.getTopicProposal(this.token, id);
+        if (topic) {
+          this.title = topic.title;
+          this.code = topic.code;
+          this.description = topic.description;
+          this.limit = topic.limit;
+          if (topic.scheduleId) {
+            this.scheduleId = topic.scheduleId;
+            this.listSchedules = [topic.schedule];
+          }
+          if (topic.lecturerId) this.lecturerId = topic.lecturerId._id;
+          this.studentIds = topic.students;
+          this.listStudentsSelected = topic.list_students;
+          // if (this.currentScheduleId) this.$store.commit('topic_proposal/setTopicScheduleId', this.currentScheduleId);
         }
-        this.studentIds = topic.students;
-        this.scheduleId = this.currentScheduleId;
-        if (this.currentScheduleId) this.$store.commit('topic_proposal/setTopicScheduleId', this.currentScheduleId);
       }
+    } catch (e) {
+      console.error(e);
     }
   },
   methods: {
+    chooseStudent () {
+      this.showSelectStudent = true;
+    },
     rollBack () {
       this.$store.dispatch('url/updateSection', `${this.module}-list`);
     },
@@ -236,11 +359,21 @@ export default {
         this.$toast.error('Số lượng thành viên không quá 3 thành viên và không nhỏ hơn 1');
         return false;
       }
-      if (this.studentIds.length > this.limit) {
+      if (this.studentIds.length !== this.limit) {
         this.$toast.error('Số lượng sinh viên được chọn không được quá số lượng giới hạn');
         return false;
       }
       return true;
+    },
+
+    changeStudents (students) {
+      this.showSelectStudent = false;
+      if (students.length !== Number(this.limit)) {
+        this.$toast.error('Số lượng sinh viên được chọn phải bằng số lượng giới hạn!');
+        return;
+      }
+      this.listStudentsSelected = students;
+      this.studentIds = this.listStudentsSelected.map((st) => st.code);
     },
   },
 };
