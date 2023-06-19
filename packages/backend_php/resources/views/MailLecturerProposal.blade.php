1eb7ec5610884fc741e0f9783252fc14786cf461
Hung Pham <t.hungpham2314@gmail.com>
2023-06-19 22:12:48 +0700

    [BE] - topic proposal - send email when student create proposal

--- packages/backend_php/resources/views/MailLecturerProposal.blade.php
+++ packages/backend_php/resources/views/MailLecturerProposal.blade.php
@@ -1,2 +1,5 @@
-<h1>Hi, {{ data_get($data, 'name') }}</h1>
-<p>Sending Mail using Lumen.</p>
+<h2>Đợt đăng ký: {{ data_get($data, 'topic.schedule.code') }} - {{ data_get($data, 'topic.schedule.name') }}.</h2>
+<h3>Người đề xuất: {{ data_get($data, 'topic.createdBy.code') }} - {{ data_get($data, 'topic.createdBy.name') }}</h3>
+<p>Đề tài: {{ data_get($data, 'topic.title') }} </p>
+<p>Mô tả: {{ data_get($data, 'topic.description') }}</p>
+<p>Quý thầy cô vui lòng truy cập http://hungpq.click để xem thông tin chi tiết</p>
